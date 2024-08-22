348b752d2a403a91ab7692f45e73221e4d6f95f7
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-08-22 07:20:43 -0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // Configuración personalizada de CORS
+  app.enableCors({
+    origin: '*', // Permitir solicitudes solo desde este origen
+    methods: 'GET,HEAD', // Métodos HTTP permitidos
+    credentials: true, // Permitir que las cookies se incluyan en las solicitudes
+  });
+
   await app.listen(3000);
 }
 bootstrap();
