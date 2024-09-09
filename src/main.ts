27f15a5680d55eb310b935e385590a8cba208842
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-09-09 10:32:27 -0300

    added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync(join(__dirname, '../certificates/server.key')),
+    cert: readFileSync(join(__dirname, '../certificates/server.crt')),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD', 
